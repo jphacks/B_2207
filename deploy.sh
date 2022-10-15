efe9e9cb4ef6c57424c0b7e85257b0225d0925db
Mishima-Ryuji <ryuji.mishima.com@gmail.com>
2022-10-16 02:58:04 +0900

    🎨 ディレクトリの構造を変更してデプロイ用の設定を追加

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+poetry export -f requirements.txt --output ../requirements.txt
+git add .
+git commit -m "deploy to heroku"
+heroku login
+heroku git:remote -a jp-hacks-2022
+git push heroku dev:main
