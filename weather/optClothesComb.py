d01c8600334b11eb885f283ebf3bf48af1351331
19-yamamoto <daigomaru1255@gmail.com>
2022-10-16 16:13:04 +0900

    天気APIから体感温度を取得

--- weather/optClothesComb.py
+++ weather/optClothesComb.py
@@ -1,7 +1,6 @@
 # 体感温度と服の特徴量から最適な服を選ぶ
 
-import weather # weather.pyから体感温度の最低値(min_feel_tmps)を取得
-import featFromClothes # featFromClothes.pyから服の特徴量を取得(予定)
+import weather  # weather.pyから体感温度の最低値(min_feel_tmps)を取得
 
 min_feel_tmps = weather.min_feel_tmps # 体感温度の最低値
 
