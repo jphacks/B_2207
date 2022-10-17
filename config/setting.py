870fef55f0443a4f3b7b5de3ef1dc76cbba6d775
19-yamamoto <daigomaru1255@gmail.com>
2022-10-17 17:43:16 +0900

    変更

--- config/setting.py
+++ config/setting.py
@@ -1,5 +1,7 @@
 
 
+import os
+
 import firebase_admin
 from dotenv import load_dotenv
 from firebase_admin import credentials, firestore
@@ -8,10 +10,10 @@ from firebase_admin import credentials, firestore
 class Setting:
     def __init__(self):
         # .envから環境変数を読み込む
-        load_dotenv("./config/.env")
+        load_dotenv(".env")
 
         # Firebaseから設定値を取得
-        cred = credentials.Certificate("./config/jphacks-2022-firebase-adminsdk-ijn6v-e2108c052f.json")
+        cred = credentials.Certificate("jphacks-2022-firebase-adminsdk-ijn6v-e2108c052f.json")
         firebase_admin.initialize_app(cred)
         self.__db = firestore.client()
 
@@ -19,13 +21,19 @@ class Setting:
         # .envに環境変数を追加した場合はSlackで全メンバーに伝えること
         # 以下のhogeのように環境変数を取得する
         # self.__hoge = os.environ.get("HOGE")
+        self.__wapi = os.environ.get("API_KEY")
 
     @property
     def db(self):
         return self.__db
-
+    
+    @property 
+    def wapi(self):
+        return self.__wapi
     # 環境変数を追加したらpropertyにも追加すること
     # これによりsettingで参照できるようになる
     # @property
     # def hoge(self):
     #     return self.__hoge
+
+#print(Setting().wapi)
---
+++ recommender/searcher.py
@@ -0,0 +1,5 @@
+# 体感温度と服の特徴量から最適な服を選ぶ
+
+import weatherAPI # weatherAPI.pyから体感温度の最低値(min_feel_tmps)を取得
+
+min_feel_tmps = weatherAPI.min_feel_tmps # 体感温度の最低値
