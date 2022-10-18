26b2761ff14d58fbad29dbc4b751b53dc7227219
arahi10 <sorahebi0717@gmail.com>
2022-10-18 15:00:51 +0900

    deploy

--- minor_change.sh
+++ minor_change.sh
@@ -1,7 +1,5 @@
 poetry export -f requirements.txt --output requirements.txt
 git add .
 git commit -m "deploy to heroku"
-# heroku login
-heroku git:remote -a jp-hacks-2022
 git push heroku feat/LINEhook:main
-echo https://jp-hacks-2022.herokuapp.com/
+heroku open
