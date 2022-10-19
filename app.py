9a44f2b355a5c85ee2d517d69a5b1e47d02ab3c7
arahi10 <sorahebi0717@gmail.com>
2022-10-19 12:41:01 +0900

    deploy to heroku

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import datetime
 import subprocess
+import os
 from flask import Flask, abort, request
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from config.envloader import Envloader
 
 CONNECT_TIMEOUT,READ_TIMEOUT = 10,60*5
 app = Flask(__name__)
 subprocess.run(['find','-name','*.env'])
 # load this environment's .env
 # loader = Envloader()
-# print(loader.env.channel_secret == "a676780a4132f1a41e5cab58d4b17674")
-# print(loader.env.channel_access_token == "z+wwUFo23oaC1J9rxPOJY0cyFy7m0pB6ty6D2w077NVXKuVUpthMBTgQpCDQSZyMDF/WjPKHh9AdUHTxo2RutRulMIresZaPLGwN06VyxNVFX/fSjbucMnP0dgtsJnHFu2w/HaEPx3diJeCOJIXW+QdB04t89/1O/w1cDnyilFU=")
+print(os.environ["CHANNEL_SECRET"] == "a676780a4132f1a41e5cab58d4b17674")
+print(os.environ["CHANNEL_ACCESS_TOKEN"] == "z+wwUFo23oaC1J9rxPOJY0cyFy7m0pB6ty6D2w077NVXKuVUpthMBTgQpCDQSZyMDF/WjPKHh9AdUHTxo2RutRulMIresZaPLGwN06VyxNVFX/fSjbucMnP0dgtsJnHFu2w/HaEPx3diJeCOJIXW+QdB04t89/1O/w1cDnyilFU=")
 # line_bot_api = LineBotApi("z+wwUFo23oaC1J9rxPOJY0cyFy7m0pB6ty6D2w077NVXKuVUpthMBTgQpCDQSZyMDF/WjPKHh9AdUHTxo2RutRulMIresZaPLGwN06VyxNVFX/fSjbucMnP0dgtsJnHFu2w/HaEPx3diJeCOJIXW+QdB04t89/1O/w1cDnyilFU=",timeout=1000)
 # handler = WebhookHandler("a676780a4132f1a41e5cab58d4b17674")
 line_bot_api = LineBotApi("z+wwUFo23oaC1J9rxPOJY0cyFy7m0pB6ty6D2w077NVXKuVUpthMBTgQpCDQSZyMDF/WjPKHh9AdUHTxo2RutRulMIresZaPLGwN06VyxNVFX/fSjbucMnP0dgtsJnHFu2w/HaEPx3diJeCOJIXW+QdB04t89/1O/w1cDnyilFU=",timeout=(CONNECT_TIMEOUT,READ_TIMEOUT))
--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ git add .
 git commit -m "deploy to heroku"
 # heroku login # 初回は必要,一度ログインしたら要らない
 heroku git:remote -a jp-hacks-2022
-git push heroku feat/LINEhook:main
 # heroku側の環境変数を.envファイルをもとに設定, `heroku plugins:install heroku-config` が必要
 heroku config:push --file=config/.env
+git push heroku feat/LINEhook:main
 
 echo https://jp-hacks-2022.herokuapp.com/
