85841e3a318178bea794d9c6d19e1f25cc1bdf8f
arahi10 <sorahebi0717@gmail.com>
2022-10-19 10:49:09 +0900

    deploy to heroku

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from config.envloader import Envloader
 
 app = Flask(__name__)
 # load this environment's .env
-loader = Envloader()
-line_bot_api = LineBotApi(loader.env.channel_access_token)
-handler = WebhookHandler(loader.env.channel_secret)
+# loader = Envloader()
+line_bot_api = LineBotApi("z+wwUFo23oaC1J9rxPOJY0cyFy7m0pB6ty6D2w077NVXKuVUpthMBTgQpCDQSZyMDF/WjPKHh9AdUHTxo2RutRulMIresZaPLGwN06VyxNVFX/fSjbucMnP0dgtsJnHFu2w/HaEPx3diJeCOJIXW+QdB04t89/1O/w1cDnyilFU=")
+handler = WebhookHandler("a676780a4132f1a41e5cab58d4b17674")
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
