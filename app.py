bbb32d58ea17b9572175390143f37c37b56b83df
arahi10 <sorahebi0717@gmail.com>
2022-10-19 11:58:28 +0900

    deploy to heroku

--- app.py
+++ app.py
@@ -3,20 +3,25 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
+from config.envloader import Envloader
+
 
 app = Flask(__name__)
 # load this environment's .env
-# loader = Envloader()
+loader = Envloader()
+print(loader.env.channel_secret == "a676780a4132f1a41e5cab58d4b17674")
+print(loader.env.channel_access_token == "z+wwUFo23oaC1J9rxPOJY0cyFy7m0pB6ty6D2w077NVXKuVUpthMBTgQpCDQSZyMDF/WjPKHh9AdUHTxo2RutRulMIresZaPLGwN06VyxNVFX/fSjbucMnP0dgtsJnHFu2w/HaEPx3diJeCOJIXW+QdB04t89/1O/w1cDnyilFU=")
+# line_bot_api = LineBotApi("z+wwUFo23oaC1J9rxPOJY0cyFy7m0pB6ty6D2w077NVXKuVUpthMBTgQpCDQSZyMDF/WjPKHh9AdUHTxo2RutRulMIresZaPLGwN06VyxNVFX/fSjbucMnP0dgtsJnHFu2w/HaEPx3diJeCOJIXW+QdB04t89/1O/w1cDnyilFU=",timeout=1000)
+# handler = WebhookHandler("a676780a4132f1a41e5cab58d4b17674")
+
 line_bot_api = LineBotApi("z+wwUFo23oaC1J9rxPOJY0cyFy7m0pB6ty6D2w077NVXKuVUpthMBTgQpCDQSZyMDF/WjPKHh9AdUHTxo2RutRulMIresZaPLGwN06VyxNVFX/fSjbucMnP0dgtsJnHFu2w/HaEPx3diJeCOJIXW+QdB04t89/1O/w1cDnyilFU=")
 handler = WebhookHandler("a676780a4132f1a41e5cab58d4b17674")
-# @app.route("/")
-# def hello_world():
-#     return "<p>ohaohaoha, World!</p>"
+@app.route("/")
+def hello_world():
+    return "<p>ohaohaoha, World!</p>"
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    app.logger.info("callback ")
-    print("callback ")
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
@@ -36,8 +41,6 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    app.logger.info("handle_message ")
-    print("mh")
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
