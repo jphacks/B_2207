781a058fa71c24d330a739b59aa1a00cbcc44076
Mishima-Ryuji <ryuji.mishima.com@gmail.com>
2022-10-20 00:48:40 +0900

    🎉 環境構築

---
+++ src/main/java/jp/ac/titech/onolab/aidiscovery/AidiscoveryApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.titech.onolab.aidiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AidiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ac/titech/onolab/aidiscovery/AidiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.titech.onolab.aidiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
