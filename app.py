3a5004cbf1c8d0f407cb0ec6ca89608757c6db81
arahi10 <sorahebi0717@gmail.com>
2022-10-19 12:18:57 +0900

    deploy to heroku

--- app.py
+++ app.py
@@ -2,21 +2,18 @@ from flask import Flask, abort, request
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-
 from config.envloader import Envloader
-import subprocess
 
+CONNECT_TIMEOUT,READ_TIMEOUT = 10,60*5
 app = Flask(__name__)
 # load this environment's .env
-s = subprocess.run(['ls','-l'],stdout=subprocess.PIPE)
-print(s.stdout)
-# loader = Envloader()
-# print(loader.env.channel_secret == "a676780a4132f1a41e5cab58d4b17674")
-# print(loader.env.channel_access_token == "z+wwUFo23oaC1J9rxPOJY0cyFy7m0pB6ty6D2w077NVXKuVUpthMBTgQpCDQSZyMDF/WjPKHh9AdUHTxo2RutRulMIresZaPLGwN06VyxNVFX/fSjbucMnP0dgtsJnHFu2w/HaEPx3diJeCOJIXW+QdB04t89/1O/w1cDnyilFU=")
+loader = Envloader()
+print(loader.env.channel_secret == "a676780a4132f1a41e5cab58d4b17674")
+print(loader.env.channel_access_token == "z+wwUFo23oaC1J9rxPOJY0cyFy7m0pB6ty6D2w077NVXKuVUpthMBTgQpCDQSZyMDF/WjPKHh9AdUHTxo2RutRulMIresZaPLGwN06VyxNVFX/fSjbucMnP0dgtsJnHFu2w/HaEPx3diJeCOJIXW+QdB04t89/1O/w1cDnyilFU=")
 # line_bot_api = LineBotApi("z+wwUFo23oaC1J9rxPOJY0cyFy7m0pB6ty6D2w077NVXKuVUpthMBTgQpCDQSZyMDF/WjPKHh9AdUHTxo2RutRulMIresZaPLGwN06VyxNVFX/fSjbucMnP0dgtsJnHFu2w/HaEPx3diJeCOJIXW+QdB04t89/1O/w1cDnyilFU=",timeout=1000)
 # handler = WebhookHandler("a676780a4132f1a41e5cab58d4b17674")
 
-line_bot_api = LineBotApi("z+wwUFo23oaC1J9rxPOJY0cyFy7m0pB6ty6D2w077NVXKuVUpthMBTgQpCDQSZyMDF/WjPKHh9AdUHTxo2RutRulMIresZaPLGwN06VyxNVFX/fSjbucMnP0dgtsJnHFu2w/HaEPx3diJeCOJIXW+QdB04t89/1O/w1cDnyilFU=",timeout=(10,60*3))
+line_bot_api = LineBotApi("z+wwUFo23oaC1J9rxPOJY0cyFy7m0pB6ty6D2w077NVXKuVUpthMBTgQpCDQSZyMDF/WjPKHh9AdUHTxo2RutRulMIresZaPLGwN06VyxNVFX/fSjbucMnP0dgtsJnHFu2w/HaEPx3diJeCOJIXW+QdB04t89/1O/w1cDnyilFU=",timeout=(CONNECT_TIMEOUT,READ_TIMEOUT))
 handler = WebhookHandler("a676780a4132f1a41e5cab58d4b17674")
 @app.route("/")
 def hello_world():
@@ -43,6 +40,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print(f"message text is '{event.message.text}'")
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
